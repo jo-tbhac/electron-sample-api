69d16807d9b55e88ca21cabcca4ed63d68658d11
junpei ogawa <cornerstone.am3@gmail.com>
2020-09-25 15:58:54 +0900

    add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_many :group_users
+  has_many :groups, through: :group_users
+
   has_secure_password validations: true
 
   validates :email, presence: true, uniqueness: true
